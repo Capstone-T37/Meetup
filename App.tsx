7444172741d09321c19f89f3ad874b680df59ca1
ayman fakri <61848289+afakri@users.noreply.github.com>
2023-01-18 20:12:41 -0500

    implemented navigation (#22)

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import React, { type PropsWithChildren } from 'react';
 import { SafeAreaView, StyleSheet, Button, Text } from 'react-native';
 import { store } from './src/redux/store'
 import { Provider } from 'react-redux'
-import Counter from './src/components/Counter';
+import BottomNavBar from './src/components/BottomNavBar';
+
 
 const App = () => {
   return (
-    <SafeAreaView >
+    <SafeAreaView style={{ height: '100%' }}>
       <Provider store={store}>
-      <Counter />
+        <BottomNavBar />
       </Provider>
     </SafeAreaView>
   );
