629bc135c7188388ee4c78a216987232a60c0f2c
ayman fakri <61848289+afakri@users.noreply.github.com>
2023-01-22 18:10:01 -0500

    Profile page (#30)
    * implemented
    
    * started profile sec
    
    * finished profile page

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -5,6 +5,9 @@ import React from 'react'
 import Home from '../screens/Home';
 import CounterScreen from '../screens/CounterScreen';
 import AntDesign from 'react-native-vector-icons/AntDesign'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import ProfileScreen from '../screens/ProfileScreen';
+
 type Props = {}
 
 const BottomNavBar = (props: Props) => {
@@ -12,9 +15,10 @@ const BottomNavBar = (props: Props) => {
 
     return (
         <NavigationContainer >
-            <Tab.Navigator >
-                <Tab.Screen name="Home" component={Home} options={{tabBarIcon:()=>(<AntDesign name='home'/>)}}/>
-                <Tab.Screen name="Counter" component={CounterScreen} options={{tabBarIcon:()=>(<AntDesign name='clockcircleo'/>)}}/>
+            <Tab.Navigator screenOptions={{headerShown: false}}>
+                <Tab.Screen name="Profile" component={ProfileScreen} options={{ tabBarIcon: () => (<MaterialCommunityIcons name='account-circle-outline' size={23} />) }} />
+                <Tab.Screen name="Home" component={Home} options={{ tabBarIcon: () => (<AntDesign name='home' size={23} />) }}  />
+                <Tab.Screen name="Counter" component={CounterScreen} options={{ tabBarIcon: () => (<AntDesign name='clockcircleo' size={23} />) }}  />
             </Tab.Navigator>
         </NavigationContainer>
     )
