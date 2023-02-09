93d70bf181da0661846febc14df6a3d657786365
RiyadElazhari <59661625+riyad123456@users.noreply.github.com>
2023-02-08 19:07:50 -0500

    android issue fixed

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,5 @@
 export const routes = {
     login: '/api/v1/login',
     signup: '/api/v1/signup',
-    localhost: 'http://localhost:4000'
+    localhost: 'https://0a6d-2607-fea8-bb5c-aae0-696c-9a5f-983-c395.ngrok.io'
 }
