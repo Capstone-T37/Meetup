41e1d57046757318ab098e3bbed23e55870ea1b8
RiyadElazhari <59661625+riyad123456@users.noreply.github.com>
2023-02-08 20:16:27 -0500

    lil fix

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,5 @@
 export const routes = {
     login: '/api/v1/login',
     signup: '/api/v1/signup',
-    localhost: 'https://0a6d-2607-fea8-bb5c-aae0-696c-9a5f-983-c395.ngrok.io'
+    localhost: 'https://b470-2605-b100-d03-805f-a467-c032-e6e5-3927.ngrok.io'
 }
